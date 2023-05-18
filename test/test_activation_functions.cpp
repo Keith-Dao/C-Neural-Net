a15ad6c0204ac7bc484ba764825080a149729870
Keith Dao <keith.dao.248@gmail.com>
2023-05-18 16:14:49 +1000

    Refactors activation function tests to remove duplicated tests.

--- test/test_activation_functions.cpp
+++ test/test_activation_functions.cpp
@@ -1,96 +1,95 @@
 #include "activation_functions.hpp"
 #include <Eigen/Dense>
 #include <gtest/gtest.h>
-#include <iostream>
 
 using namespace activation_functions;
 
-#pragma region NoActivation
-class TestNoActivation : public ::testing::Test {
-protected:
-  NoActivation noActivation;
-  Eigen::MatrixXd
-      X = Eigen::VectorXd::LinSpaced(20, -10, 9).reshaped(4, 5).transpose(),
-      Y = Eigen::MatrixXd(X), grad = Eigen::MatrixXd::Ones(X.rows(), X.cols());
+#pragma region Fixture
+class FixtureData {
+public:
+  std::shared_ptr<ActivationFunction> function;
+  Eigen::MatrixXd X, Y, grad;
+
+  FixtureData(std::shared_ptr<ActivationFunction> function, Eigen::MatrixXd X,
+              Eigen::MatrixXd Y, Eigen::MatrixXd grad)
+      : function(function), X(X), Y(Y), grad(grad){};
 };
-
-TEST_F(TestNoActivation, Test_Call) {
-  ASSERT_TRUE(noActivation(X).isApprox(Y)) << "Operator:\n"
-                                           << X << "\n"
-                                           << Y << "\n";
+// Display a human readable name for the fixture data.
+std::ostream &operator<<(std::ostream &os, FixtureData const &fixture) {
+  return os << fixture.function->getName();
 }
 
-TEST_F(TestNoActivation, Test_Forward) {
-  ASSERT_TRUE(noActivation.forward(X).isApprox(Y)) << "Forward:\n"
-                                                   << X << "\n"
-                                                   << Y << "\n";
+class Functions : public testing::TestWithParam<FixtureData> {};
+#pragma endregion Fixture
+
+#pragma region Tests
+TEST_P(Functions, Test_Call) {
+  auto [function, X, Y, _] = GetParam();
+  decltype(X) originalX(X);
+  ASSERT_TRUE((*function)(X).isApprox(Y)) << "Operator:\n"
+                                          << typeid(function).name() << "\n"
+                                          << X << "\n"
+                                          << Y << "\n";
+  ASSERT_TRUE(originalX.isApprox(X))
+      << "Operation was done inplace but should not have been.";
 }
 
-TEST_F(TestNoActivation, Test_Backward) {
-  noActivation(X);
-  ASSERT_TRUE(noActivation.backward().isApprox(grad)) << "Backward:\n"
-                                                      << X << "\n"
-                                                      << Y << "\n";
+TEST_P(Functions, Test_Forward) {
+  auto [function, X, Y, _] = GetParam();
+  decltype(X) originalX(X);
+  ASSERT_TRUE(function->forward(X).isApprox(Y))
+      << "Operator:\n"
+      << typeid(function).name() << "\n"
+      << X << "\n"
+      << Y << "\n";
+  ASSERT_TRUE(originalX.isApprox(X))
+      << "Operation was done inplace but should not have been.";
 }
 
-TEST_F(TestNoActivation, Test_Backward_Before_Forward) {
-  EXPECT_THROW(noActivation.backward(), BackwardBeforeForwardException);
+TEST_P(Functions, Test_Backward) {
+  auto [function, X, Y, grad] = GetParam();
+  (*function)(X);
+  ASSERT_TRUE(function->backward().isApprox(grad))
+      << "Backward:\n"
+      << typeid(function).name() << "\n"
+      << X << "\n"
+      << grad << "\n";
 }
 
-TEST_F(TestNoActivation, Test_Equal) {
-  ASSERT_EQ(noActivation, NoActivation())
-      << "All no activation functions should equal.";
-  ASSERT_NE(noActivation, ReLU())
-      << "A no activation function is not a ReLU function.";
-}
-#pragma endregion NoActivation
-
-#pragma region ReLU
-class TestReLU : public ::testing::Test {
-protected:
-  ReLU relu;
-  Eigen::MatrixXd
-      X = Eigen::VectorXd::LinSpaced(20, -10, 9).reshaped(4, 5).transpose(),
-      Y = Eigen::MatrixXd{{0, 0, 0, 0},
-                          {0, 0, 0, 0},
-                          {0, 0, 0, 1},
-                          {2, 3, 4, 5},
-                          {6, 7, 8, 9}},
-      grad = Eigen::MatrixXd{
-          {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 1}, {1, 1, 1, 1}, {1, 1, 1, 1},
-      };
-};
-
-TEST_F(TestReLU, Test_Call) {
-  Eigen::MatrixXd *originalX = &X;
-  ASSERT_TRUE(relu(X).isApprox(Y)) << "Operator:\n" << X << "\n" << Y << "\n";
-  ASSERT_FALSE(X.isApprox(Y) && originalX == &X)
-      << "Operation was done inplace.\n";
+TEST_P(Functions, Test_Backward_Before_Forward) {
+  std::shared_ptr<ActivationFunction> function = GetParam().function;
+  EXPECT_THROW(function->backward(), BackwardBeforeForwardException);
 }
 
-TEST_F(TestReLU, Test_Forward) {
-  Eigen::MatrixXd *originalX = &X;
-  ASSERT_TRUE(relu.forward(X).isApprox(Y)) << "Forward:\n"
-                                           << X << "\n"
-                                           << Y << "\n";
-  ASSERT_FALSE(X.isApprox(Y) && originalX == &X)
-      << "Operation was done inplace.\n";
+TEST_P(Functions, Test_Equal) {
+  std::shared_ptr<ActivationFunction> function = GetParam().function;
+  ASSERT_EQ(function->getName() == ReLU().getName(), *function == ReLU());
+  ASSERT_EQ(function->getName() == NoActivation().getName(),
+            *function == NoActivation());
 }
+#pragma endregion Tests
 
-TEST_F(TestReLU, Test_Backward) {
-  relu(X);
-  ASSERT_TRUE(relu.backward().isApprox(grad)) << "Backward:\n"
-                                              << X << "\n"
-                                              << Y << "\n";
-}
+#pragma region Data
+FixtureData noActivationData(
+    std::make_shared<NoActivation>(),
+    Eigen::VectorXd::LinSpaced(20, -10, 9).reshaped(4, 5).transpose(),
+    Eigen::VectorXd::LinSpaced(20, -10, 9).reshaped(4, 5).transpose(),
+    Eigen::MatrixXd::Ones(5, 4)),
+    reluData(std::make_shared<ReLU>(),
+             Eigen::VectorXd::LinSpaced(20, -10, 9).reshaped(4, 5).transpose(),
+             Eigen::MatrixXd{{0, 0, 0, 0},
+                             {0, 0, 0, 0},
+                             {0, 0, 0, 1},
+                             {2, 3, 4, 5},
+                             {6, 7, 8, 9}},
+             Eigen::MatrixXd{
+                 {0, 0, 0, 0},
+                 {0, 0, 0, 0},
+                 {0, 0, 0, 1},
+                 {1, 1, 1, 1},
+                 {1, 1, 1, 1},
+             });
 
-TEST_F(TestReLU, Test_Backward_Before_Forward) {
-  EXPECT_THROW(relu.backward(), BackwardBeforeForwardException);
-}
-
-TEST_F(TestReLU, Test_Equal) {
-  ASSERT_EQ(relu, ReLU()) << "All ReLU functions should equal.";
-  ASSERT_NE(relu, NoActivation())
-      << "A ReLU function is not a no activation function.";
-}
-#pragma endregion ReLU
+INSTANTIATE_TEST_SUITE_P(TestActivationFunctions, Functions,
+                         ::testing::Values(noActivationData, reluData));
+#pragma endregion Data
