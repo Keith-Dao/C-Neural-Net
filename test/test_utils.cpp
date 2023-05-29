3a815cacbf87d41050459746097c35aa62f8436c
Keith Dao <keith.dao.248@gmail.com>
2023-05-30 00:09:16 +1000

    Adds tests for expections in from_json.

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,3 +1,4 @@
+#include "exceptions.hpp"
 #include "utils.hpp"
 #include <Eigen/Dense>
 #include <gtest/gtest.h>
@@ -43,5 +44,36 @@ INSTANTIATE_TEST_SUITE_P(
                                  json{{1}, {1.5}, {2}, {2.5}, {3}}),
                       MatrixData({}, json::array())));
 
+TEST(MatrixUtils, TestJsonToMatrixWithInvalidValues) {
+  json values = json::parse(R"([1, 2])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONArray2DException)
+      << "1D arrays are not supported, only 2D.";
+
+  values = json::parse(R"([[[1, 2]]])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONArray2DException)
+      << "Higher dimension arrays are not supported, only 2D.";
+}
+
+TEST(MatrixUtils, TestJsonToMatrixWithInvalidTypes) {
+  json values = json::parse(R"([[true, false]])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONTypeException)
+      << "Booleans are not supported, only numbers.";
+
+  values = json::parse(R"([["a", "b"]])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONTypeException)
+      << "Strings are not supported, only numbers.";
+
+  values = json::parse(R"([[null]])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONTypeException)
+      << "null is not supported, only numbers.";
+
+  values = json::parse(R"([[{"a": "b"}, {"b": "c"}]])");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONTypeException)
+      << "JSON objects are not supported, only numbers.";
+
+  values = json::parse(R"({"a": "b"})");
+  EXPECT_THROW(from_json(values), src_exceptions::JSONTypeException)
+      << "JSON objects are not supported, only 2D.";
+}
 #pragma endregion Matrices
 } // namespace test_utils
